f925614aab9660c8bd405b2ab60cfeed7e8bda19
Dan Elias <deli9382@uni.sydney.edu.au>
2018-02-06 10:56:34 +1100

    scalability tests; cleanup

--- adjacent_block_index.py
+++ adjacent_block_index.py
@@ -1,15 +1,13 @@
-from itertools import product
-
 import numpy as np
 import pandas as pd
 
 from recordlinkage.indexing import BlockIndex
 
 def ranking1d(x):
-    ndx_by_rank = np.argsort(x)
-    rank_by_ndx = np.empty_like(ndx_by_rank)
-    rank_by_ndx[ndx_by_rank] = np.arange(len(ndx_by_rank))
-    return rank_by_ndx
+    x_by_rank = np.sort(np.unique(x))
+    result = np.searchsorted(x_by_rank, x).astype(float)
+    result[np.isnan(x)] = np.nan
+    return result
 
 class AdjacentBlockIndex(BlockIndex):
     '''
@@ -17,71 +15,72 @@ class AdjacentBlockIndex(BlockIndex):
     includes matches with elements in adjacent blocks
     and null values
     '''
-    def __init__(self, *args, max_nulls=0, **kwargs):
+    def __init__(self, *args, max_nulls=0, ndx_sorting_keys=slice(None), **kwargs):
         super().__init__(*args, **kwargs)
         self.max_nulls = max_nulls
-    def _link_index(self, df_a, df_b):
-        default_on = list(set(df_a.columns) & set(df_b.columns))
+        self.ndx_sorting_keys = slice(0,0) if ndx_sorting_keys is None else ndx_sorting_keys
+    def _key_dfs(self, *dfs):
+        '''
+        Normalized dataframes.  Properties:
+        * same indices as originals
+        * columns restricted to blocking keys
+        * Non-null values of blocking keys replaced with integer ranks
+        * column names normalized
+        Returns:
+            list: key dfs
+        '''
+        assert 1 <= len(dfs) <= 2
+        default_on = [c for c in dfs[0].columns if all(c in df.columns for df in dfs)]
         raw_key_columns = [side_on or self.on or default_on for side_on in [self.left_on, self.right_on]]
         if not (all(raw_key_columns) and (len(raw_key_columns[0]) == len(raw_key_columns[1]))):
-            raise IndexError('Invalid indexing columns')
-        key_dfs = [df[side_on].copy() for df, side_on in zip([df_a, df_b], raw_key_columns)]
+            raise IndexError('Invalid blocking keys')
+        key_dfs = [df[side_on].copy() for df, side_on in zip(dfs, raw_key_columns)]
         key_columns = ['k_{ndx}'.format(**locals()) for ndx in range(len(key_dfs[0].columns))]
         for df in key_dfs:
             df.columns = key_columns
-        def get_key_rank(key_series):
-            unique_values = key_series.unique()
-            result = pd.Series(ranking1d(unique_values), index=unique_values)
-            if np.nan in result:
-                result[np.nan] = np.nan
-            return result
-        key_ranks = {name: get_key_rank(vals) 
-                     for name, vals in pd.concat(key_dfs, ignore_index=True).iteritems()}
-        index_columns = ['index_{ndx}'.format(**locals()) for ndx in range(len(key_dfs))]
-        for index_column, key_df in zip(index_columns, key_dfs):
-            key_df[index_column] = key_df.index
-            for col in key_columns:
-                key_df[col] = key_ranks[col][key_df[col].values].values
-        def get_block_df(key_df, df_id):
-            blocks = key_df[key_columns].drop_duplicates().reset_index(drop=True)
-            blocks.index.name = 'block_id_{df_id}'.format(**locals())
-            return blocks
-        block_dfs = [get_block_df(key_df, df_id) for df_id, key_df in enumerate(key_dfs)]
-        key_dfs = [key_df.join(block_df[key_columns].reset_index().set_index(key_columns), on=key_columns)
-                   for key_df, block_df in zip(key_dfs, block_dfs)]
-        def df_with_adjusted_nulls(df):
-            df = df[df[key_columns].isnull().sum(axis='columns') <= self.max_nulls]
-            for col in key_columns:
-                df[col].fillna(-2, inplace=True)            
-            return df
-        def get_adjusted_block_df(block_df_to_adjust, non_null_adjustments):
-            rank_increments = [non_null_adjustments+[np.nan] if (self.max_nulls > 0) and (np.nan in key_ranks[col]) else non_null_adjustments
-                               for col in key_columns]
-            rank_increment_combinations = np.array([incs.flatten() for incs in np.meshgrid(*rank_increments)])
-            rank_increment_combinations = rank_increment_combinations[:,(np.sum(np.isnan(rank_increment_combinations), axis=0)<=self.max_nulls)]
-            ix_repeated_rows = np.tile(np.arange(len(block_df_to_adjust)), rank_increment_combinations.shape[1])
-            result = pd.DataFrame((block_df_to_adjust[key_columns].values[...,np.newaxis] 
-                                   + rank_increment_combinations[np.newaxis,...]).transpose([2,0,1]).reshape(-1,len(key_columns)),
-                                  columns=key_columns,
-                                  index = block_df_to_adjust.index[ix_repeated_rows])
-            result = result.join(block_df_to_adjust[[c for c in block_df_to_adjust.columns if c not in result.columns]])
-            result = result.reset_index().drop_duplicates().set_index(block_df_to_adjust.index.name)
-            result = df_with_adjusted_nulls(result)
-            return result
-        ext_block_dfs = [get_adjusted_block_df(df, non_null_adjustments=[-1,0,1])
-                         for df, adjs in zip(block_dfs, [[-1,0,1], [0]])]
-        adj_block_dfs = [df_with_adjusted_nulls(df) for df in block_dfs]
-        block_match_components = [df0.reset_index().join(df1.reset_index().set_index(key_columns), on=key_columns, how='inner')
-                                  for df0, df1 in zip(ext_block_dfs, reversed(adj_block_dfs))]
-        block_matches = pd.concat(block_match_components, ignore_index=True)
-        block_matches = block_matches.loc[(block_matches[key_columns]<0).sum(axis='columns').values<=self.max_nulls, :]
-        block_id_columns = [df.index.name for df in block_dfs]
-        block_matches = block_matches[block_id_columns].drop_duplicates()
-        record_index_dfs = [df[[c for c in df.columns if c in index_columns or c in block_id_columns]]
-                            for df in key_dfs]
-        record_matches = record_index_dfs[0]\
-                         .merge(block_matches, on=block_id_columns[0])\
-                         .merge(record_index_dfs[1], on=block_id_columns[1])
-        record_matches = record_matches[index_columns].drop_duplicates()
-        record_matches = record_matches[record_matches.iloc[:,0] != record_matches.iloc[:,1]]
-        return record_matches.set_index(index_columns).index.rename([df.index.name for df in key_dfs])
+        for col, vals in pd.concat(key_dfs, ignore_index=True).iteritems():
+            unique_vals = vals.unique()
+            key_ranks = pd.Series(ranking1d(unique_vals), index=unique_vals)
+            for key_df in key_dfs:
+                key_df[col] = key_ranks[key_df[col].values].values
+        return key_dfs
+    def _inclusive_key_df_link_index(self, *key_dfs):
+        '''
+        link index for dataframe(s) of the form produced by _key_dfs, including "matches" of a record to itself
+        Args:
+            *key_dfs: 1 or 2 dataframes of the form produced by _key_dfs
+        Returns:
+            :class:`pandas.MultiIndex`: 
+        '''
+        assert (1 <= len(key_dfs) <= 2) and all(tuple(df.columns) == tuple(key_dfs[0].columns) for df in key_dfs)
+        blocks = pd.concat(key_dfs, ignore_index=True).drop_duplicates().reset_index(drop=True)
+        blocks = blocks[blocks.isnull().sum(axis='columns') <= self.max_nulls].reset_index(drop=True)
+        df_unique_block_ids = [blocks.index.values] if len(key_dfs) == 1 else [key_df.merge(blocks.reset_index(), on=list(key_df.columns))['index'].unique() for key_df in key_dfs]
+        if blocks.max().max() in [0, np.nan]:
+            block_pair_candidates = pd.MultiIndex.from_product([df_unique_block_ids[0], df_unique_block_ids[-1]])
+        else:
+            coarsened_blocks = np.floor(blocks.copy() / 2)
+            block_pair_candidates = self._inclusive_key_df_link_index(*[coarsened_blocks.loc[block_ids, :] for block_ids in df_unique_block_ids])
+        rank_difference_components = [blocks.loc[block_pair_candidates.get_level_values(lvl),:].reset_index(drop=True) for lvl in range(2)]
+        rank_differences = rank_difference_components[0].copy() - rank_difference_components[1]
+        rank_differences.index = block_pair_candidates
+        del rank_difference_components
+        def key_subset(sorting):
+            all_keys = key_dfs[0].columns
+            sorting_keys = list(all_keys[self.ndx_sorting_keys])
+            return sorting_keys if sorting else [c for c in all_keys if c not in sorting_keys]
+        null_count_ok = rank_differences.isnull().sum(axis='columns') <= self.max_nulls
+        sorting_keys_match = not(key_subset(sorting=True)) or rank_differences[key_subset(sorting=True)].abs().max(axis='columns').isin([0, 1, np.nan])
+        non_sorting_keys_match = not(key_subset(sorting=False)) or rank_differences[key_subset(sorting=False)].abs().max(axis='columns').isin([0, np.nan])
+        block_link = rank_differences[null_count_ok & sorting_keys_match & non_sorting_keys_match].index.to_frame().reset_index(drop=True)
+        block_link.columns = ['block_id_a', 'block_id_b']
+        block_id_lookup = blocks.reset_index(drop=False).set_index(list(blocks.columns)).iloc[:,0]
+        block_id_lookup.name = 'block_id'
+        block_record_id_mappings = [key_df.join(block_id_lookup, on=block_id_lookup.index.names)[[block_id_lookup.name]].reset_index() for key_df in key_dfs]
+        return block_record_id_mappings[0].rename(columns={'index':'index_a'}).merge(block_link, left_on='block_id', right_on='block_id_a')[['index_a', 'block_id_b']].merge(block_record_id_mappings[-1].rename(columns={'index':'index_b'}), left_on='block_id_b', right_on='block_id').set_index(['index_a', 'index_b']).index
+    def _dedup_index(self, df_a):
+        result = self._inclusive_key_df_link_index(*self._key_dfs(df_a))
+        result = result[result.get_level_values(0) < result.get_level_values(1)]
+        return result
+    def _link_index(self, df_a, df_b):
+        return self._inclusive_key_df_link_index(*self._key_dfs(df_a, df_b))
---
+++ scalability.py
@@ -0,0 +1,202 @@
+
+# coding: utf-8
+
+# # Index method scalability comparison
+# 
+# This notebook compares the scalability properties of AdjacentBlockIndex with the BlockIndex and SortedNeighbourhoodIndex (both in the recordlinkage package).  
+# 
+# 
+# 
+# Note: it should be run with memory paging disabled.  To do this on Linux, use:
+# 
+# ```
+# sudo watch --interval 500 swapoff -a
+# ```
+# 
+# Also, the kernel frequently gets killed, but the test function can continue from save points it makes as it goes.  A good way of restarting automatically (on Linux) is to export the notebook to a .py file
+# (say, scalability.py) and then run:
+# 
+# ```
+# while [ ! -f timings.pickle ]; do python scalability.py ; done
+# ```
+# 
+
+# In[ ]:
+
+
+#from collections import ChainMap
+from functools import lru_cache, reduce
+from itertools import product
+from operator import mul
+import datetime, inspect, pathlib
+import numpy as np
+import pandas as pd
+
+
+from recordlinkage import FullIndex, BlockIndex, SortedNeighbourhoodIndex
+from adjacent_block_index import AdjacentBlockIndex
+
+from experiment_helpers import sample_dedup_dataset
+
+
+# ## Testing function
+
+# In[ ]:
+
+
+def get_index_timings(index_details, row_counts, column_counts, distinct_entity_instance_counts, granularities, index_length_limit=5e6, index_time_limit=1, result_file=None, save_interval_seconds=60, verbose=False, debug=False):
+    '''
+    Negative granularities are neighbourhood radii
+    '''
+    if result_file is not None:
+        result_file = pathlib.Path(result_file).resolve()
+        partial_result_file = result_file.parent.joinpath('_{}'.format(result_file.name))
+        latest_keys_file = partial_result_file.parent.joinpath('_{}'.format(partial_result_file.name))
+    timings = pd.read_pickle(str(partial_result_file)) if (result_file is not None) and partial_result_file.exists() else pd.DataFrame()
+    failed_keys = pd.read_pickle(str(latest_keys_file)) if (result_file is not None) and latest_keys_file.exists() else {}
+    last_save_time = datetime.datetime.now()
+    def msg(*args, **kwargs):
+        if verbose:
+            print(*args, **kwargs)
+    def debug_msg(*args, **kwargs):
+        if debug:
+            print(*args, **kwargs)
+    total_combinations = reduce(mul, map(len, [index_details, row_counts, column_counts, distinct_entity_instance_counts, granularities]), 1)
+    combinations_processed = progress = 0
+    id_cols = ['instances_per_entity', 'granularity', 'rows', 'columns', 'label']
+    for instances_per_entity in sorted(distinct_entity_instance_counts):
+        msg('Instances per entity: {instances_per_entity}'.format(**locals()))
+        @lru_cache(maxsize=1)
+        def largest_continuous_dataset():
+            msg('Computing continuous dataset')
+            return sample_dedup_dataset(rows=max(row_counts), columns=max(column_counts), instances_per_entity=instances_per_entity)
+        for granularity in sorted(granularities, reverse=True):
+            msg('{progress:.0%}\tGranularity: {granularity}'.format(**locals()))
+            @lru_cache(maxsize=1)
+            def largest_discrete_dataset():
+                msg('\tDiscretizing dataset')
+                return np.floor(largest_continuous_dataset() * granularity)
+            for rows, columns in product(sorted(row_counts), sorted(column_counts)):
+                msg('{progress:.0%}\t\tTable: {rows} x {columns}'.format(**locals()))
+                @lru_cache(maxsize=2)
+                def dataset(dataset_type):
+                    largest_dataset = {'continuous': largest_continuous_dataset, 'discrete':largest_discrete_dataset}[dataset_type]
+                    return largest_dataset().iloc[:rows, :columns]
+                length_full = rows * (rows-1) // 2
+                for label, index_type, index_kwargs in index_details:
+                    index_kwargs = dict(index_kwargs)
+                    combinations_processed += 1
+                    progress = combinations_processed / total_combinations
+                    _locals = locals()
+                    current_keys = {k:_locals[k] for k in id_cols}
+                    def omission_contitions():
+                        debug_msg('\t\t\t\t{granularity} inapplicable to {label}?'.format(**locals()))
+                        yield ((index_type is not SortedNeighbourhoodIndex) and (granularity < 0))
+                        debug_msg('\t\t\t\t{granularity} granularity exceeds {rows} rows?'.format(**locals()))
+                        yield granularity > rows
+                        debug_msg('\t\t\t\t{columns} Too many columns?'.format(**locals()))
+                        yield ((index_type is SortedNeighbourhoodIndex) and (columns > 1))
+                        debug_msg('\t\t\t\tmax nulls too high?'.format(**locals()))
+                        yield index_kwargs.get('max_nulls', columns) > columns
+                        if len(timings) > 0:
+                            debug_msg('\t\t\t\tthis config already done?'.format(**locals()))
+                            yield reduce((lambda a,b: a&b), (timings[c]==_locals[c] for c in id_cols)).any()
+                            if index_type is FullIndex:
+                                debug_msg('\t\t\t\t{rows} rows already done for {label}?'.format(**locals()))
+                                yield (timings[timings['label']==label]['rows'] == rows).any()
+                            if index_type is SortedNeighbourhoodIndex:
+                                debug_msg('\t\t\t\t{rows} rows {granularity} granularity already done for {label}?'.format(**locals()))
+                                yield ((timings[timings['label']==label]['rows'] == rows) & (timings[timings['label']==label]['granularity'] == granularity)).any()
+                            comparison_rows = reduce((lambda a,b: a&b), 
+                                                     (timings[c]==_locals[c] for c in id_cols if c != 'rows'),
+                                                     timings['rows'] <= rows,
+                                                    )
+                            if comparison_rows.any():
+                                comparison_maxima = timings[comparison_rows][['elapsed_seconds', 'index_length', 'memory_overflow']].max()
+                                debug_msg('\t\t\t\tsmaller index produced memory overflow?')
+                                yield comparison_maxima['memory_overflow'] 
+                                if index_time_limit is not None:
+                                    debug_msg('\t\t\t\tsmaller index already exceeds time limit?')
+                                    yield comparison_maxima['elapsed_seconds'] > index_time_limit
+                                if index_length_limit is not None:
+                                    debug_msg('\t\t\t\tsmaller index already exceeds length limit?')
+                                    yield comparison_maxima['index_length'] > index_length_limit
+                    if any(omission_contitions()):
+                        debug_msg ('\t\t\t\t\tskipping {label}'.format(**locals()))
+                        continue
+                    msg('{progress:.0%}\t\t\t{label}'.format(**locals()))
+                    stats_dict = {k: _locals[k] for k in (['length_full'] + id_cols)}
+                    stats_dict.update(index_kwargs)
+                    if index_type is SortedNeighbourhoodIndex:
+                        dataset_type = 'continuous'
+                        index_kwargs['window'] = 1 + 2 * (int(rows / granularity / 2) if granularity > 0 else -granularity)
+                    else:
+                        dataset_type = 'discrete'
+                        stats_dict.update({k: _locals[k] for k in ['granularity']})
+                    if 'on' in inspect.signature(index_type).parameters:
+                        index_kwargs['on'] = list(dataset(dataset_type).columns)
+                    indexer = index_type(**index_kwargs)
+                    memory_overflow = (current_keys == failed_keys)
+                    elapsed_seconds = index_length = np.nan
+                    if not memory_overflow:
+                        if result_file is not None:
+                            pd.to_pickle(current_keys, str(latest_keys_file))
+                        try:
+                            start_time = datetime.datetime.now()
+                            index_length = len(indexer.index(dataset(dataset_type)))
+                            elapsed_seconds = (datetime.datetime.now() - start_time).total_seconds()
+                        except MemoryError:
+                            memory_overflow = True
+                    stats_dict.update({'index_length': index_length,
+                                       'elapsed_seconds': elapsed_seconds,
+                                       'memory_overflow': memory_overflow,
+                                      })
+                    timings = timings.append(stats_dict, ignore_index=True)
+                    if (result_file is not None) and (datetime.datetime.now() - last_save_time).total_seconds() > save_interval_seconds:
+                        timings.to_pickle(str(partial_result_file))
+                        last_save_time = datetime.datetime.now()
+    timings['reduction_ratio'] = 1 - timings['index_length'] / timings['length_full']
+    if result_file is not None:
+        timings.to_pickle(str(partial_result_file))
+        partial_result_file.rename(result_file)
+    progress = 1
+    msg('{progress:.0%}\t Done'.format(**locals()))
+    return timings
+
+
+# ## Execution
+
+# In[ ]:
+
+
+index_details = [#(label, type, additional kwargs),
+                 ('Full', FullIndex, {}),
+                 ('Standard Blocking',BlockIndex , {}),
+                 ('Sorted Neighbourhood', SortedNeighbourhoodIndex, {}),
+                 ('Adjacent Blocking - Standard Blocking settings',AdjacentBlockIndex , {'max_nulls': 0, 'ndx_sorting_keys':None}),
+                 ('Adjacent Blocking - no wildcards',AdjacentBlockIndex , {'max_nulls': 0}),
+                 ('Adjacent Blocking - 1 wildcard',AdjacentBlockIndex , {'max_nulls': 1}),
+                 ('Adjacent Blocking - 2 wildcards',AdjacentBlockIndex , {'max_nulls': 2}),
+                ]
+
+def int_geomspace(start, stop, num=50, endpoint=True):
+    return np.unique(np.logspace(np.log10(start), np.log10(stop), num=num, endpoint=endpoint, dtype=int))
+
+timings = get_index_timings(index_details=index_details,
+                        row_counts = int_geomspace(start=10, stop=1000000, num=16),
+                        column_counts = 1 + np.arange(10),
+                        distinct_entity_instance_counts = int_geomspace(start=1, stop=100, num=5),
+                        granularities = (-int_geomspace(start=1, stop=100, num=10)).tolist() + int_geomspace(start=1, stop=1000, num=10).tolist(),
+                        index_length_limit = 10e6,
+                        index_time_limit = 30,
+                        result_file = pathlib.Path('timings.pickle'),
+                        verbose = True,
+                        debug = False,
+                        )
+
+
+# In[ ]:
+
+
+
+
